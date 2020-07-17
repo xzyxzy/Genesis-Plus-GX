c2d382f4d2b52854b4635c2484cd20afbd1483cd
heyjoeway <joseph14078@gmail.com>
2020-07-17 00:44:41 -0400

    shit

--- src/backends/sound/sound_null.c
+++ src/backends/sound/sound_null.c
@@ -1,12 +1,12 @@
 #include "sound_base.h"
 
-int Backend_Sound_Close() { return 1; }
 int Backend_Sound_Init() { return 1; }
 int Backend_Sound_Update(int size) { return 1; }
-int Backend_Sound_Pause() { return 1; }
-
+int Backend_Sound_Close() { return 1; }
 int Backend_Sound_PlayMusic(char *path) { return 1; }
 int Backend_Sound_StopMusic() { return 1; }
 int Backend_Sound_FadeOutMusic(int fadeTime) { return 1; }
-int Backend_Sound_PauseMusic() { return 1; }
-int Backend_Sound_ResumeMusic() { return 1; }
+int Backend_Sound_MusicSpeed(float speed) { return 1; }
+int Backend_Sound_MusicSetUnderwater(int isUnderwater) { return 1; }
+int Backend_Sound_PlaySFX(char *path) { return 1; }
+int Backend_Sound_SetPause(int paused) { return 1; }
