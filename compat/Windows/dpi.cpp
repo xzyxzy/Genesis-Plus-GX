4ef80bd879293484754ddcd37551ce49f4a6b73f
heyjoeway <joseph14078@gmail.com>
2020-12-13 19:34:19 -0500

    Fix compilation with latest msys2 Seriously what the fuck are they doing, their packages are totally fucked right now

--- compat/Windows/dpi.cpp
+++ compat/Windows/dpi.cpp
@@ -1,6 +1,15 @@
 #include <windows.h>
+#include <stdlib.h>
 
 int main (int argc, char *argv[]);
+
+#ifndef __argc
+    extern int __argc;
+#endif
+
+#ifndef __argv
+    extern char *__argv[];
+#endif
 
 // Fuck windows and its janky ass hidpi shit
 // No, I am not including a manifest file just to stop you from fucking with scaling, microsoft
